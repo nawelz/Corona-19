3dcf2d5b854bedecd83d11d0b0b6860114e62630
Zied Maaloul <me@ziedmaaloul.site>
2020-04-05 01:30:43 +0100

    Update .editorconfig, 404.php, and 1397 more files...

--- wp-content/themes/onepage-lite/searchform.php
+++
@@ -1,6 +0,0 @@
-<form method="get" id="searchform" class="search-form" action="<?php echo home_url(); ?>" _lpchecked="1">
-	<fieldset>
-		<input type="text" name="s" id="s" value="<?php the_search_query(); ?>" placeholder="<?php _e('Search the site','onepage-lite'); ?>" />
-		<input id="search-image" class="sbutton" type="submit" value="<?php _e('Search','onepage-lite'); ?>" />
-	</fieldset>
-</form>
